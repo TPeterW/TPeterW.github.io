7c29491e776c9e75687450fcb8fb1f0af10af9a1
TPeterW <peterwangtao0@hotmail.com>
2016-05-02 01:34:27 -0400

    CS453 Homework 9

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function toHw5() {
     $('#modalClassProjects').modal('hide');
     window.open('sub/class/cs453/hw5.html', '_blank');
 }
+
+function toHw9() {
+    $('#modalClassProjects').modal('hide');
+    window.open("http://www.cs.middlebury.edu/~dsilver/vision/feature-tracking");
+}
